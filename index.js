89c7ab8cdaf7346be74873cabfc2a796691e8410
superwann32 <142965165+superwann32@users.noreply.github.com>
2024-03-20 16:57:01 +0100

    navbar end

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-
+const links = document.querySelectorAll("nav li")
+const main = document.querySelector("main")
 
 icons.addEventListener("click", () => {
     nav.classList.toggle("active")
 })
+
+links.forEach((link) => {
+    link.addEventListener('click', () => {
+        nav.classList.remove("active")
+    })
+})
+
+main.addEventListener('click', () => {
+    nav.classList.remove("active")
+})
+
+
 
 
 function openModale() { }
