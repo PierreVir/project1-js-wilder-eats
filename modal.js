99a3215202944bfb25fa0f21a244fa4035930964
Romain <97295039+RomainRG31@users.noreply.github.com>
2024-03-21 19:05:09 +0100

    add heartFill onClicl

--- modal.js
+++ modal.js
@@ -1,6 +1,5 @@
 const modalContainer = document.querySelector(".modal-container");
 const modalTrigger = document.querySelectorAll(".modal-trigger");
-const modalContainerTrigger = document.querySelector(".heart-icon");
 
 modalTrigger.forEach((trigger) =>
   trigger.addEventListener("click", toggleModal)
@@ -8,3 +7,31 @@ modalTrigger.forEach((trigger) =>
 function toggleModal() {
   modalContainer.classList.toggle("active");
 }
+
+// Fonction de notation étoiles
+document.addEventListener("DOMContentLoaded", function () {
+  const stars = document.querySelectorAll(".opinion-stars");
+  stars.forEach((star) => {
+    star.addEventListener("click", function () {
+      const rating = this.getAttribute("data-rating");
+      updateStars(rating);
+    });
+  });
+
+  function updateStars(rating) {
+    stars.forEach((star) => {
+      if (star.getAttribute("data-rating") <= rating) {
+        star.src = "img/starFill.svg";
+      } else {
+        star.src = "img/starts.svg";
+      }
+    });
+  }
+});
+// Fonction de remplissage du coeur
+document.addEventListener("DOMContentLoaded", function () {
+  const heart = document.querySelector(".heart-icon");
+  heart.addEventListener("click", function () {
+    heart.src = "img/hearthFill.svg";
+  });
+});
