455740e05c80903637906d0e348148064cd74ed8
PierreVir <pierre.virama@gmail.com>
2024-03-22 10:53:30 +0100

    Informations added in restaurant.js

--- restaurants.js
+++ restaurants.js
@@ -23,15 +23,25 @@ let restaurants = [{
     itinerary:"https://www.google.com/maps/dir//10+Rue+du+Pont+Montaudran,+31000+Toulouse/@43.5987115,1.3731763,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x12aebc8e3307c495:0x427126caf2fd8f51!2m2!1d1.455577!2d43.5987411?hl=fr&entry=ttu",
     description:"La rotisserie pour ceux qui veulent se manger un bon poulet a la rotisserie vous pourriez vous y rendre à moin de 5min a pied de la Wild de Toulouse",
     openingHours:"Lundi: 10h00-14h00 & 16h00-21h00, Mardi: 10h00-14h00 & 16h00-21h00, Mercredi: 10h00-14h00 & 16h00-21h00, Jeudi: 10h00-14h00 & 16h00-21h00, Vendredi: 10h00-14h00 & 16h00-21h00",
-    walkTime: "a 5 minutes"
+    walkTime: "à 5 minutes"
 },
 {
     name:"Carrefour city",
     type:"Superette",
+    adress: "1 Place A. Bernard, 31000 Toulouse",
+    itinerary:"https://www.google.com/search?q=carrefour+city+grand+rond+toulouse&rlz=1C1ONGR_frFR1053FR1053&oq=carrefour+city+grand+rond+toulouse&gs_lcrp=EgZjaHJvbWUyCQgAEEUYORiABNIBCDg5MzlqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8#vhid=/g/1thlq3px&vssid=lclsmap",
+    description:"tout ce qu'on a besoin c'est carrefour city tu veux du café pour la Wild il y en a, tu veux du sucre pour accompagner ton café c'est aussi possible tu veux te faire un sandwich avec plusieurs ingrédients c'est aussi possible la superette peut vraiment te sortir d'une faim incroyable",
+    openingHours:"Lundi: 07h00-21h00, Mardi: 07h00-21h00, Mercredi: 07h00-21h00, Jeudi: 07h00-21h00, Vendredi: 07h00-21h00",
+    walkTime: "à 2 minutes"
 },
 {
     name: "Le kiosque",
     type: "asiatique",
+    adress:"77 Allée Forain-François Verdier, 31000 Toulouse",
+    itinerary:"https://www.google.com/maps/dir//77+All.+Forain-Fran%C3%A7ois+Verdier,+31000+Toulouse/@43.5968365,1.3699023,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x12aebc859c7ecc03:0x568c00fa0109c281!2m2!1d1.4523084!2d43.5968283?entry=ttu",
+    description:"Pour les Fins gourmet de restaurant asiatique vous avez le Kiosque",
+    openingHours:"Lundi: 11h00-22h00, Mardi: 11h00-22h00, Mercredi: 11h00-22h00, Jeudi: 11h00-22h00, Vendredi: 11h00-22h00.",
+    walkTime:"à 4 minutes"
 }
 
 
